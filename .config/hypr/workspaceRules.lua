5bc9fcdedb01fe23b2c2990a9130a23a17e8a8c1
Ayrikws <ayrik.ws@proton.me>
2026-06-10 16:50:43 +0330

    workspaceRules.lua update

--- .config/hypr/workspaceRules.lua
+++ .config/hypr/workspaceRules.lua
@@ -1,18 +1,15 @@
--- Special Workspaces
 hl.workspace_rule({ workspace = "special:overview", gaps_out = 80 })
-hl.workspace_rule({ workspace = "special:running", no_rounding = true, gaps_out = 80 })
+hl.workspace_rule({ workspace = "special:running", gaps_out = 80 })
 
--- Distinct Layout overrides
 hl.workspace_rule({ workspace = "2", border_size = 4, gaps_out = 20 })
 hl.workspace_rule({ workspace = "4", gaps_out = 50 })
 
--- Batch generate standard layouts (1, 3, 5, 6, 7, 8)
 local standard_workspaces = { "1", "3", "5", "6", "7", "8" }
 for _, ws in ipairs(standard_workspaces) do
     hl.workspace_rule({
         workspace = ws,
-        gaps_out = (ws == "1") and 20 or 30, -- Workspace 1 gets 20, others get 30
-        gaps_in = (ws ~= "3") and 10 or nil, -- Workspace 3 skips gaps_in
+        gaps_out = (ws == "1") and 20 or 30, 
+        gaps_in = (ws ~= "3") and 10 or nil, 
         border_size = 3
     })
 end
