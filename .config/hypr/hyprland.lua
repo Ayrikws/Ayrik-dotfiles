eabcf352f72e23d3acc0bf38b1f7be6f146288f7
Ayrikws <ayrik.ws@proton.me>
2026-06-15 13:14:41 +0330

    hyprland.lua update

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -32,7 +32,7 @@ end)
 ---- ENVIRONMENT VARIABLES ----
 -------------------------------
 hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
-hl.env("XCURSOR_SIZE", "22")
+hl.env("XCURSOR_SIZE", 22)
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
 ---------------------
@@ -48,45 +48,52 @@ hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 -----------------------
 hl.config({
     general = {
-        gaps_in = 3,
-        gaps_out = 5,
-        border_size = 2,
+        gaps_in = 8,
+        gaps_out = 20,
+
+        border_size = 1,
+
         col = {
-            active_border = "rgb(EBDBB2)",
-            inactive_border = "rgba(00000000)"
-        },
+            active_border   = "rgb(EBDBB2)",
+            inactive_border = "rgba(00000000)",
+	},
+
         resize_on_border = true,
-        allow_tearing = false,
-        layout = "scrolling"
+        allow_tearing 	 = false,
+        layout 			 = "scrolling"
     },
 
     decoration = {
-        rounding = 15,
-        active_opacity = 1.0,
-        inactive_opacity = 0.85,
+        rounding 	   	 = 15,
+        active_opacity 	 = 1.0,
+        inactive_opacity = 0.9,
+
         shadow = {
-            enabled = true,
-            range = 4,
+            enabled 	 = true,
+            range 		 = 4,
             render_power = 3,
-            color = "rgba(1a1a1aee)"
+            color 		 = "rgba(0a0a0a89)",
+
         },
+
         blur = {
             enabled = true,
-            size = 5,
-            passes = 2,
+            size 	= 5,
+            passes 	= 2,
+
             new_optimizations = true,
-            ignore_opacity = true,
-            xray = false,
-            contrast = 0.9,
+            ignore_opacity 	  = true,
+            xray 			  = false,
+
+            contrast   = 0.9,
             brightness = 0.8,
-            vibrancy = 0.18
+            vibrancy   = 0.18
         }
     },
 
-
     misc = {
         force_default_wallpaper = 0,
-        disable_hyprland_logo = false
+        disable_hyprland_logo   = false
     },
 
     input = {
@@ -100,7 +107,6 @@ hl.config({
         }
     },
     
-    -- Restored the scrolling layout rules!
     scrolling = {
         column_width = 0.65,
         fullscreen_on_one_column = true
